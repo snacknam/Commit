11f446671029fead5aa7e54cc64d375ded20baa6
Kwansik Nam <kwansiknam@gmail.com>
2024-01-09 21:47:52 +0900

    Initial Commit

---
+++ Commit/CommitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitApp.swift
+//  Commit
+//
+//  Created by 관식 on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CommitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
