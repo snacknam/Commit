8c8ba550bd060de95cb6be13d46e7df77bddff3c
Kwansik Nam <kwansiknam@gmail.com>
2024-01-09 23:02:56 +0900

    랜덤 색상 적용

--- Commit/CommitsView.swift
+++ Commit/CommitsView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct CommitsView: View {
     var body: some View {
-        
         let columns = [
             GridItem(.flexible()),
             GridItem(.flexible()),
@@ -20,16 +19,43 @@ struct CommitsView: View {
             GridItem(.flexible())
         ]
         
-        VStack(alignment: .leading) {
-            Text("commits")
-                .font(.system(size: 72, weight: .thin))
-            LazyVGrid(columns: columns) {
-                ForEach(0..<28, id: \.self) { index in
-                    Circle()
-                        .foregroundStyle(.white)
+        ZStack {
+            Color(.background)
+                .ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 0) {
+                Text("9 January")
+                    .font(.system(size: 24, weight: .light))
+                    .foregroundStyle(Color(.gray0))
+                Text("commits")
+                    .font(.system(size: 64, weight: .thin))
+                    .frame(height: 56)
+                    .padding(.bottom, 32)
+                LazyVGrid(columns: columns) {
+                    ForEach(0..<35, id: \.self) { index in
+                        RoundedRectangle(cornerRadius: 16)
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(Color.randomCommitColor())
+                    }
                 }
+                Spacer()
             }
+            .padding(.horizontal, 8)
+            .padding(.top, 24)
         }
+    }
+}
+
+extension Color {
+    static let commitColors = [
+        Color("commit0"),
+        Color("commit1"),
+        Color("commit2"),
+        Color("commit3"),
+        Color("commit4")
+    ]
+    
+    static func randomCommitColor() -> Color {
+        commitColors.randomElement() ?? .gray
     }
 }
 
